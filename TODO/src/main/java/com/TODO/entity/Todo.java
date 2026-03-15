15f08d9e84dc57037daf8c120a20d4249f212780
vivek <vivekreddy3225@gmail.com>
2026-03-16 00:00:49 +0530

    Setup DB connection and update Todo entity

--- TODO/src/main/java/com/TODO/entity/Todo.java
+++ TODO/src/main/java/com/TODO/entity/Todo.java
@@ -1,7 +1,20 @@
 package com.TODO.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private boolean completed;
+    private LocalDate dueDate;
+    private LocalDateTime createdAt;
 }
