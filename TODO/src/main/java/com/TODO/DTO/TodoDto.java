aca8d58e9aafe9edf25b130d6c447bb5ef612bdb
vivek <vivekreddy3225@gmail.com>
2026-03-21 23:13:23 +0530

    Added Dto class

---
+++ TODO/src/main/java/com/TODO/DTO/TodoDto.java
@@ -0,0 +1,67 @@
+package com.TODO.DTO;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TodoDto {
+    private Integer id;
+    private String description;
+    private boolean completed;
+    private LocalDate dueDate;
+    private LocalDateTime createdAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", completed=" + completed +
+                ", dueDate=" + dueDate +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+
+    public TodoDto() {
+
+    }
+}
